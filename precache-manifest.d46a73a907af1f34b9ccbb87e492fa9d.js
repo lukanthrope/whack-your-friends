2e54980cf0c35b7b72fea4800a9239105d55a6ca
lukanthrope <bydlocoder228@gmail.com>
2018-12-28 18:05:19 +0200

    Updates

---
+++ precache-manifest.d46a73a907af1f34b9ccbb87e492fa9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c72512e4b8a72a3f4af",
+    "url": "/whack-your-friends/static/js/runtime~main.1c72512e.js"
+  },
+  {
+    "revision": "a6667773a9fa773d279b",
+    "url": "/whack-your-friends/static/js/main.a6667773.chunk.js"
+  },
+  {
+    "revision": "623daecb7ae6398f6696",
+    "url": "/whack-your-friends/static/js/1.623daecb.chunk.js"
+  },
+  {
+    "revision": "a6667773a9fa773d279b",
+    "url": "/whack-your-friends/static/css/main.e7f6eb08.chunk.css"
+  },
+  {
+    "revision": "623daecb7ae6398f6696",
+    "url": "/whack-your-friends/static/css/1.64ef009f.chunk.css"
+  },
+  {
+    "revision": "65c7d500bae03ff4da0561e404b6b6e1",
+    "url": "/whack-your-friends/index.html"
+  }
+];
